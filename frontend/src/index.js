7f953e4c49fd597e0ecec5d1c9054e664a5b22d9
jake <jake.is.mybrother@gmail.com>
2022-07-21 16:23:22 +0900

    htmlの雛形

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,36 @@
 import React from "react";
 import reactDOM from "react-dom";
 import Editor from "rich-markdown-editor";
+import styled from "styled-components";
 
 
-function App () {
+//TODO 一覧のタイトルをクリックして、内容を表示
+
+
+/**
+ * 一覧から読み込んだノートの内容を、defaultValue にセットする
+ */
+const App = () => {
   return (
-    <>
-      <h1>Cloud Note Application</h1>
-      <Editor />
+    <>      
+      <TitleArea placeholder="タイトル" />
+      <Editor 
+        placeholder="何か入力してください" 
+        defaultValue="" 
+        dark={false}
+      />
     </>
   );
 };
 
-const root = document.getElementById("root");
-reactDOM.render(<App />, root);
+
+
+const TitleArea = styled.input`
+  width: 200px;
+  font-size: 16px;
+  margin: 10px;
+`;
+
+// ID に 変更
+// Target Container is not a DOM Element 
+reactDOM.render(<App />, document.getElementById("editor"));
