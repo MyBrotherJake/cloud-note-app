a20e3eb52dd73729f91e4ba480f33bde82c55abc
hisato yoneda <h.yoneda@day-1.jp>
2022-08-03 17:10:05 +0900

    add: repositoryの追加

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(),
+    NotesModule
+  ],
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
