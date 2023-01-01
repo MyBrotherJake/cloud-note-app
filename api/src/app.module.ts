23ec717c1d4cd09a5e51ac5ab5d7ec554c5d8edd
da-yone <h.yoneda@day-1.jp>
2023-01-01 13:03:47 +0900

    prisma用コードベース作成

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { NotesModule } from './notes/notes.module';
-import { UsersModule } from './users/users.module';
-import { FoldersModule } from './folders/folders.module';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(),
-    ConfigModule.forRoot({ isGlobal: true }),
-    NotesModule,
-    UsersModule,
-    FoldersModule,
-    AuthModule
-  ],
-  controllers: [],
-  providers: []
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- api/src/types.ts
+++
@@ -1,7 +0,0 @@
-import { Folder } from './entities/folder.entity';
-import { Note } from './entities/note.entity';
-
-export interface FoldersAndNotes {
-  folders: Folder[],
-  notesWithoutFolder: Note[]
-}
